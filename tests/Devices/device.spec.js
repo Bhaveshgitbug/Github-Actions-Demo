452ff23120104067960ca10438f8ea10428d2a28
Bhavesh <bhavesh.kathuria@bughunters.io>
2022-08-23 14:54:10 +0530

    changes done

--- tests/Devices/device.spec.js
+++ tests/Devices/device.spec.js
@@ -1,19 +1,19 @@
-// @ts-nocheck
+// // @ts-nocheck
 
-const { test, devices, chromium } = require('@playwright/test')
-const iphone = devices['iPhone 11'];
-/* Devices return the dictionary of devices available*/
+// const { test, devices, chromium } = require('@playwright/test')
+// const iphone = devices['iPhone 11'];
+// /* Devices return the dictionary of devices available*/
 
-test('test devices', async() => {
-    const browser = await chromium.launch({headless : false, slowMo : 300})
-    const context =  browser.newContext({
-       ...iphone,
-       permissions : ['geoloaction'],
-       geolocation :  {latitude : 28.458791529463006, longitude : 77.30577858465894},
-       locale : 'hi-IN'
-    })
-    const page = await context.newPage()
-    await page.goto('https://www.google.com/maps')
-    await page.waitForTimeout(5000)
-    await browser.close()
-})
+// test('test devices', async() => {
+//     const browser = await chromium.launch({headless : true, slowMo : 300})
+//     const context =  browser.newContext({
+//        ...iphone,
+//        permissions : ['geoloaction'],
+//        geolocation :  {latitude : 28.458791529463006, longitude : 77.30577858465894},
+//        locale : 'hi-IN'
+//     })
+//     const page = await context.newPage()
+//     await page.goto('https://www.google.com/maps')
+//     await page.waitForTimeout(5000)
+//     await browser.close()
+// })
